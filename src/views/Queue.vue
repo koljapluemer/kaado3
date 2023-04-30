caae2fb70638d30826273a5eda13a5769f14acb9
blackbrokkoli <kastomat@gmail.com>
2023-04-30 15:11:32 +0200

    build reveal flow

--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -1,21 +1,37 @@
 <script setup>
+  import { ref, onMounted } from 'vue'
   import { storeToRefs } from 'pinia'
   import { useCardsStore } from '../stores/cards'
 
   const store = useCardsStore()
+
+  // reveal logic
+  const isRevealed = ref(false)
+  function review() {
+    isRevealed.value = false
+    store.getNewQueueCard()
+  }
 </script>
 
 <template>
-  <h2>Queue</h2>
-  <div>
-    <p v-if="store.getQueueCard">
-      {{ store.getQueueCard.front }}
-    </p>
+  <div class="p1" >
+    <div v-if="store.getQueueCard" class="p2 border">
+      <div id="front">
+        {{ store.getQueueCard.front }}
+      </div>
+      <div id="back" v-if="isRevealed">
+        <hr>
+        {{ store.getQueueCard.back }}
+      </div>
+    </div>
     <p v-else>
       cards loading...
     </p> 
-    <button @click="queueCard">
+    <button class="mt2" @click="review" v-if="isRevealed">
       Show Next
+    </button>
+    <button class="mt2" @click="isRevealed = !isRevealed" v-else>
+      Reveal
     </button>
   </div>
 
