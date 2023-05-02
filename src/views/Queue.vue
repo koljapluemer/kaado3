b04ed5413fff08c374ec1b1e3411450e1aab8a98
blackbrokkoli <kastomat@gmail.com>
2023-05-02 16:38:03 +0200

    improve queue logic

--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -12,7 +12,7 @@ const store = useCardsStore()
 
 // reveal logic
 const isRevealed = ref(false)
-function review() {
+function review(feedback) {
   isRevealed.value = false
   store.getNewQueueCard()
 }
@@ -20,9 +20,13 @@ function review() {
 
 <template>
   <div class="p1 flex justify-center flex-column items-center max-width-4 ml-auto mr-auto">
-    <div v-if="store.getQueueCard" class="p2 border width-full fit" style="width: 100%">
+  
+    <div id="card" v-if="store.getQueueCard" class="p2 border width-full fit" style="width: 100%">
+      <div id="card-info">
+      <code>{{ store.getQueueCard.type }}</code>
+    </div>
       <Markdown id="front" :source="store.getQueueCard.front" />
-      <div class="" v-if="isRevealed">
+      <div class="" v-if="isRevealed && store.getQueueCard.type == 'learn'">
         <hr>
         <Markdown id="back" v-if="isRevealed" :source="store.getQueueCard.back" />
       </div>
@@ -30,11 +34,119 @@ function review() {
     <p v-else>
       cards loading...
     </p>
-    <button class="mt2" @click="review" v-if="isRevealed">
-      Show Next
-    </button>
-    <button class="mt2" @click="isRevealed = !isRevealed" v-else>
-      Reveal
-    </button>
+
+    <!-- LEARN -->
+    <div v-if="store.getQueueCard.type == 'learn'">
+      <button class="mt2" @click="isRevealed = !isRevealed" v-if="!isRevealed">
+        Reveal
+      </button>
+      <div class="flex gap" v-else>
+        <button class="mt2" @click="review('blackout')">
+          Blackout
+        </button>
+        <button class="mt2" @click="review('wrong')">
+          Wrong
+        </button>
+        <button class="mt2" @click="review('incorrect-but-remembered')">
+          Incorrect But Remembered
+        </button>
+        <button class="mt2" @click="review('correct-but-hard')">
+          Correct But Hard
+        </button>
+        <button class="mt2" @click="review('good')">
+          Good
+        </button>
+      </div>
+    </div>
+
+    <!-- HABIT -->
+    <div class="flex gap" v-if="store.getQueueCard.type == 'habit'">
+      <button class="mt2" @click="review('not-today')">
+        Not Today
+      </button>
+      <button class="mt2" @click="review('do-later')">
+        Do Later
+      </button>
+      <button class="mt2" @click="review('done')">
+        Done
+      </button>
+    </div>
+
+    <!-- CHECK -->
+    <div class="flex gap" v-if="store.getQueueCard.type == 'check'">
+      <button class="mt2" @click="review('no')">
+        No
+      </button>
+      <button class="mt2" @click="review('kind-of')">
+        Kind Of
+      </button>
+      <button class="mt2" @click="review('yes')">
+        Yes
+      </button>
+    </div>
+
+    <!-- TODO -->
+    <div class="flex gap" v-if="store.getQueueCard.type == 'todo'">
+      <button class="mt2" @click="review('not-today')">
+        Not Today
+      </button>
+      <button class="mt2" @click="review('do-later')">
+        Do Later
+      </button>
+      <button class="mt2" @click="review('done')">
+        Done
+      </button>
+    </div>
+
+    <!-- MISC -->
+    <div class="flex gap" v-if="store.getQueueCard.type == 'misc'">
+      <button class="mt2" @click="review('show-next')">
+        I already knew that...
+      </button>
+      <button class="mt2" @click="review('cool-thanks')">
+        Cool, thanks!
+      </button>
+    </div>
+
+    <!-- ARTICLE -->
+    <div class="flex gap" v-if="store.getQueueCard.type == 'article'">
+      <button class="mt2" @click="review('not-today')">
+        Not Today
+      </button>
+      <button class="mt2" @click="review('do-later')">
+        Do Later
+      </button>
+      <button class="mt2" @click="review('made-some-progress')">
+        Made Some Progress
+      </button>
+      <button class="mt2" @click="review('finished')">
+        Finished
+      </button>
+    </div>
+
+    <!-- BOOK -->
+    <div class="flex gap" v-if="store.getQueueCard.type == 'book'">
+      <button class="mt2" @click="review('not-today')">
+        Not Today
+      </button>
+      <button class="mt2" @click="review('do-later')">
+        Do Later
+      </button>
+      <button class="mt2" @click="review('done')">
+        Done
+      </button>
+      <button class="mt2" @click="review('finished')">
+        Finished Book
+      </button>
+    </div>
+
+    <!-- PROJECT -->
+    <div class="flex gap" v-if="store.getQueueCard.type == 'project'">
+      <button class="mt2" @click="review('done')">
+        Ok, got it scheduled
+      </button>
+    </div>
+
+
   </div>
 </template>
--- src/views/import_cards.js
+++
@@ -1,7 +0,0 @@
-    for (const card of csvCards.value) {
-        // add properties ease (1), occurrences(0), dueAt (currentDatetime)
-        card.ease = 1
-        card.occurrences = 0
-        card.dueAt = new Date().toISOString()
-        await store.addCard(card)
-    }
