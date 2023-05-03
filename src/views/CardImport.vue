4cd8b2db991fcd7975f40face8b20faba7a4c7aa
blackbrokkoli <kastomat@gmail.com>
2023-05-03 20:13:15 +0200

    minor importer improvement/hack

--- src/views/CardImport.vue
+++ src/views/CardImport.vue
@@ -19,7 +19,7 @@ async function importCards() {
 
     <!-- fields: front, back, type, tags, is_active, is_priority, is_started, interval -->
     <vue-csv-import v-model="csvCards"
-        :fields="{ front: { required: false, label: 'Front' }, back: { required: false, label: 'Back' }, type: { required: false, label: 'Type' }, tags: { required: false, label: 'Tags' }, is_active: { required: false, label: 'Is Active' }, is_priority: { required: false, label: 'Is Priority' }, is_started: { required: false, label: 'Is Started' }, interval: { required: false, label: 'Interval' } }">
+        :fields="{ front: { required: false, label: 'Front' }, back: { required: false, label: 'Back' }, type: { required: false, label: 'Type' }, tags: { required: false, label: 'Tags' }, is_active: { required: false, label: 'Is Active' }, is_priority: { required: false, label: 'Is Priority' }, is_started: { required: false, label: 'Is Started' }, interval: { required: false, label: 'Interval'}, id: { required: false, label: 'ID' }  }">
         <vue-csv-toggle-headers></vue-csv-toggle-headers>
         <vue-csv-errors></vue-csv-errors>
         <vue-csv-input></vue-csv-input>
