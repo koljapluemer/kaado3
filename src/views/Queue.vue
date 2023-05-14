6c0f2a9275707b283554bcf14b26b8b2959c03fc
blackbrokkoli <kastomat@gmail.com>
2023-05-14 12:02:16 +0200

    allow finishing articles

--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -43,6 +43,11 @@ function review(feedback) {
     console.log('outputData', outputData)
 
   }
+
+  if (feedback == 'finished') {
+    card.type = 'misc'
+  }
+
   card.due = new Date(Date.now() + 24 * 60 * 60 * 1000)
   console.log('sending card to store: ', card)
 
